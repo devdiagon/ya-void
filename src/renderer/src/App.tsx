73130dd08b3a1a463c10ea249acb7ba6a367139e
devdiagon <fredericktimo@gmail.com>
2026-03-13 09:46:34 -0500

    fix: app not doesn't sync with DB in production build

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { RootLayout } from './layouts/RootLayout';
 
 function App(): React.JSX.Element {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <RootLayout />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
