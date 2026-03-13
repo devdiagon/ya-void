5406e0765c6102c4db764f670200b6c9003b8c8b
devdiagon <fredericktimo@gmail.com>
2026-03-13 18:07:23 -0500

    fix: auto-updater not working in production

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
 import { electronApp, is, optimizer } from '@electron-toolkit/utils'
 import { app, BrowserWindow, ipcMain, shell } from 'electron'
 import { autoUpdater } from 'electron-updater'
-import { join } from 'path'
+import path, { join } from 'path'
 import icon from '../../resources/icon.png?asset'
 import { registerIpcHandlers } from './interfaces/ipc-handlers/registerIpcHandlers'
 
@@ -23,12 +23,15 @@ function createWindow(): void {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
-    setTimeout(() => checkForUpdates(mainWindow), 1500)
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
     shell.openExternal(details.url)
     return { action: 'deny' }
+  })
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    setTimeout(() => checkForUpdates(mainWindow), 1500)
   })
 
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
@@ -40,28 +43,36 @@ function createWindow(): void {
 
 // Auto Updates - Check
 function checkForUpdates(mainWindow: BrowserWindow) {
+  
+  // Dev Only check
+  if (!app.isPackaged) {
+    autoUpdater.updateConfigPath = path.join(__dirname, '../../dev-update.yml')
+    Object.defineProperty(app, 'isPackaged', { get: () => true })
+  }
+  
   autoUpdater.autoDownload = false
-  autoUpdater.checkForUpdates()
-
+  
   autoUpdater.on('update-available', (info) => {
     mainWindow.webContents.send('update-available', info)
   })
-
+  
   autoUpdater.on('download-progress', (progress) => {
     mainWindow.webContents.send('download-progress', Math.floor(progress.percent))
   })
-
+  
   autoUpdater.on('update-downloaded', () => {
     mainWindow.webContents.send('update-downloaded')
   })
-
+  
   autoUpdater.on('error', (err) => {
     mainWindow.webContents.send('update-error', err.message)
   })
-
+  
   // Listeners from renderer
   ipcMain.on('download-update', () => autoUpdater.downloadUpdate())
   ipcMain.on('install-update', () => autoUpdater.quitAndInstall())
+
+  autoUpdater.checkForUpdates()
 }
 
 app.whenReady().then(() => {
