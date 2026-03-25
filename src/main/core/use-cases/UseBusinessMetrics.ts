f54db423f4c457448aac513392a9ed6a27ec0829
gomiiDev <carlosha176@gmail.com>
2026-03-25 14:08:56 -0500

    fix: ipc handler

--- src/main/core/use-cases/UseBusinessMetrics.ts
+++ src/main/core/use-cases/UseBusinessMetrics.ts
@@ -1,5 +1,5 @@
-import { BusinessMetrics } from '../entities/BusinessMetrics'
 import { MetricsRepository } from '../../data/repositories/MetricsRepository'
+import { BusinessMetrics } from '../entities/BusinessMetrics'
 
 export class UseBusinessMetrics {
   constructor(private metricsRepository: MetricsRepository) {}
